f2c2407ed3c0c4d931b5aa7b4a84ef47749a7d84
IanMGF <ianmarcosgomesefreitas@gmail.com>
2025-10-23 10:56:36 -0300

    feat: adjust keyboard-based movement, add camera mouse tracking

--- mechanics/control/knm_control.gd
+++ mechanics/control/knm_control.gd
@@ -2,9 +2,12 @@ extends PlayerControl
 
 @onready var camera: Camera3D = $CharacterBody3D/XROrigin/XRCamera3D
 
+const SENSITIVITY = 0.001
+
 func _ready() -> void:
 	$CharacterBody3D/XROrigin/LeftHand.queue_free()
 	$CharacterBody3D/XROrigin/RightHand.queue_free()
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func get_gun_facing() -> Vector3:
 	return camera.global_rotation
@@ -20,10 +23,16 @@ func is_holding() -> bool:
 
 func get_movement_direction() -> Vector2:
 	var cam_rotation = get_camera().global_rotation.y
-	var velocity = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var velocity = Input.get_vector("ui_left", "ui_right", "ui_down", "ui_up")
 	var unitary = velocity.rotated(cam_rotation)
 	return Vector2(unitary.x, -unitary.y)
 
 
 func get_speed() -> float:
 	return 35
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		var delta = event.relative
+		camera.global_rotation.y -= delta.x * SENSITIVITY
+		camera.global_rotation.x -= delta.y * SENSITIVITY
