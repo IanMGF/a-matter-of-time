e1327a5a6839d6c014b76f08e57af946146e4a90
IanMGF <ianmarcosgomesefreitas@gmail.com>
2025-10-27 19:23:53 -0300

    feat: add time machine model and collision

--- mechanics/button/button_press.gd
+++ mechanics/button/button_press.gd
@@ -13,6 +13,15 @@ func on_body_entered(body):
 	if body == base_body:
 		return
 	cap_mesh.position = Vector3(0, -0.125, 0)
+	var pressing_bodies = cap_area.get_overlapping_bodies()
+	if pressing_bodies.size() == 2:
+		pressed.emit()
 
 func on_body_exited(_body):
 	cap_mesh.position = Vector3.ZERO
+	var pressing_bodies = cap_area.get_overlapping_bodies()
+	if pressing_bodies.size() <= 1:
+		released.emit()
+
+signal pressed()
+signal released()
