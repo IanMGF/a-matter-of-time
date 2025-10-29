1e229db7536ca6ec16ca0814b21bc064f7cc6289
IanMGF <ianmarcosgomesefreitas@gmail.com>
2025-10-29 00:11:04 -0300

    feat: add word cubes

--- objects/Platform/magnetic-platform.gd
+++ objects/Platform/magnetic-platform.gd
@@ -8,23 +8,23 @@ var tween: Tween = null
 var locked_object = null
 
 func on_enter_area(body: RigidBody3D):
-	if not is_instance_of(body, RigidBody3D):
+	if not body.is_in_group("WordCube"):
 		return
-	
+
 	if tween:
 		tween.kill()
-	
+
 	locked_object = body
-	
+
 	# Calculate target rotation
 	var single_axis_deviation = body.global_rotation.y - self.global_rotation.y
 	var any_axis_deviation = fmod(single_axis_deviation + PI / 4, (PI / 2)) - PI / 4
 	var target_angles = Vector3(body.global_rotation)
-	
+
 	target_angles.x = 0
 	target_angles.y -= any_axis_deviation
 	target_angles.z = 0
-	
+
 	# Initiate tweens
 	tween = create_tween()
 	tween.set_parallel(true)
@@ -33,7 +33,7 @@ func on_enter_area(body: RigidBody3D):
 	tween.tween_property(body, "global_position", target_point.global_position, 1.5) \
 		.set_ease(Tween.EASE_IN_OUT) \
 		.set_trans(Tween.TRANS_CUBIC)
-	tween.tween_property(body, "rotation", target_angles, 1.5) \
+	tween.tween_property(body, "global_rotation", target_angles, 1.5) \
 		.set_ease(Tween.EASE_IN_OUT) \
 		.set_trans(Tween.TRANS_CUBIC)
 
@@ -46,25 +46,3 @@ func on_leave_area(body: PhysicsBody3D):
 func _ready() -> void:
 	attraction_area.body_entered.connect(on_enter_area)
 	attraction_area.body_exited.connect(on_leave_area)
-
-func _aphysics_process(delta: float) -> void:
-	var overlapping_bodies: Array[Node3D] = attraction_area.get_overlapping_bodies()
-	
-	# TODO: If object isn't of a specific type (word block), attraction should not happen
-	
-	for body_node: Node3D in overlapping_bodies:
-		if not is_instance_of(body_node, RigidBody3D):
-			continue
-
-		var body: RigidBody3D = body_node
-
-		var rel_position = target_point.global_position - body.global_position
-		body.global_position += min(1, delta * 5) * rel_position
-
-		var single_axis_deviation = body.global_rotation.y - self.global_rotation.y
-		var any_axis_deviation = fmod(single_axis_deviation + PI / 4, (PI / 2)) - PI / 4
-
-		body.global_rotation.y -= any_axis_deviation * delta * 5
-		body.global_rotation.x *= 0
-		body.global_rotation.z *= 0
- 
