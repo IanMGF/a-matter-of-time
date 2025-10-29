894efa6b7de61219f6471f626ec9c2f396865f5d
Nmora511 <jomoraesneto@gmail.com>
2025-10-29 18:57:16 -0300

    feat: add test scene for grab

---
+++ test_scenes/grab_test/grab_test.gd
@@ -0,0 +1,10 @@
+extends Node3D
+
+@export var player_node : PlayerControl
+
+func _ready() -> void:
+	while(true):
+		player_node.instatiate_grab_ray_Cast()
+		await get_tree().create_timer(3).timeout
+		player_node.release.emit()
+		await get_tree().create_timer(1).timeout
