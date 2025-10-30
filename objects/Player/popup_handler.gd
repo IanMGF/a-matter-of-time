fe45a01b32a1a0ed26b72bdaee4e4172b45340b9
IanMGF <ianmarcosgomesefreitas@gmail.com>
2025-10-29 22:10:11 -0300

    refactor: use get_gun_facing() instead of manual raycasting

--- objects/Player/popup_handler.gd
+++ objects/Player/popup_handler.gd
@@ -13,9 +13,8 @@ func _ready():
 
 func _process(_delta: float) -> void:
 	camera = player.get_camera()
-	var screen_center = Vector2(viewport.size.x / 2, viewport.size.y / 2)
 	var origin_point = camera.global_position
-	var normal = camera.project_ray_normal(screen_center)
+	var normal = player.get_gun_facing()
 
 	var raycast = PhysicsRayQueryParameters3D.new()
 	raycast.from = origin_point
